151ccfc446ab20db93775d3805bb450d1aa69148
Kevin McCarthy <me@kevinmccarthy.org>
2014-04-22 18:30:03 -1000

    check for old cassette

--- vcr/persist.py
+++ vcr/persist.py
@@ -1,9 +1,16 @@
 from .persisters.filesystem import FilesystemPersister
 
+def _check_for_old_cassette(cassette):
+    requests, responses = cassette
+    if requests and not hasattr(requests[0], 'uri'):
+        raise Exception("Your cassette files were generated in an older version of VCR.  Delete your cassettes or run the migration script. See http://git.io/mHhLBg for more")
+
 
 def load_cassette(cassette_path, serializer):
     with open(cassette_path) as f:
-        return serializer.deserialize(f.read())
+        cassette = serializer.deserialize(f.read())
+        _check_for_old_cassette(cassette)
+        return cassette
 
 
 def save_cassette(cassette_path, cassette_dict, serializer):
