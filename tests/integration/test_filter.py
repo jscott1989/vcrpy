18a6eac07c9eff5756679573e8cf41393a884398
Kevin McCarthy <me@kevinmccarthy.org>
2014-04-23 21:03:43 -1000

    fix string encoding for base64

--- tests/integration/test_filter.py
+++ tests/integration/test_filter.py
@@ -2,11 +2,13 @@ import base64
 from six.moves.urllib.request import urlopen, Request
 import vcr
 
+
 def _request_with_auth(url, username, password):
     request = Request(url)
-    base64string = base64.encodestring('%s:%s' % (username, password))
-    request.add_header("Authorization", "Basic %s" % base64string)
+    base64string = base64.b64encode(username.encode('ascii') + b':' + password.encode('ascii'))
+    request.add_header(b"Authorization", b"Basic " + base64string)
     return urlopen(request)
+
 
 def _find_header(cassette, header):
     for request in cassette.requests:
@@ -15,6 +17,7 @@ def _find_header(cassette, header):
                 return True
     return False
 
+
 def test_filter_basic_auth(tmpdir):
     url = 'http://httpbin.org/basic-auth/user/passwd'
     with vcr.use_cassette(str(tmpdir.join('basic_auth_filter.yaml')), filter_headers=['authorization']) as cass:
@@ -22,8 +25,9 @@ def test_filter_basic_auth(tmpdir):
         assert resp.getcode() == 200
         assert not _find_header(cass, 'authorization')
 
+
 def test_filter_querystring(tmpdir):
     url = 'http://httpbin.org/?foo=bar'
     with vcr.use_cassette(str(tmpdir.join('basic_auth_filter.yaml')), filter_query_parameters=['foo']) as cass:
-        resp = urlopen(url)
+        urlopen(url)
         assert not 'foo' in cass.requests[0].url
